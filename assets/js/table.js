2e2ca096d5d190b7a2337a9cfab8b9b285bd1de1
Daniel Heward-mills <danielheward-mills@daniel-demo-1294.local>
2019-05-08 02:17:58 -0400

    Avengers: Endgame Article Started

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,8 +1,17 @@
-    $(document).ready( function () {
+/*   $(document).ready( function () {
     $('#myTable').DataTable({
     "ajaxSource": "../assets/data/test.txt",
     "columns":[
     { "data": "name" }
+    ]
+    });
+} ); */
+
+$(document).ready( function () {
+    $('#myTable').DataTable({
+    "ajaxSource": "../assets/data/test.txt",
+    "columns":[
+    { "data": "results.bindings.name.value" }
     ]
     });
 } );
