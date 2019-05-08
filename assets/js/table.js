f8efc0b5c41006260f3ad1a4269b903050b94836
Daniel Heward-mills <danielheward-mills@dyn240.usnet.private>
2019-05-08 10:47:15 -0400

    Avengers: Endgame Article Started

--- assets/js/table.js
+++ assets/js/table.js
@@ -11,7 +11,7 @@ $(document).ready( function () {
     $('#myTable').DataTable({
     "ajaxSource": "../assets/data/survivors.json",
     "columns":[
-    { "data": "name.value" }
+    { "name": "name.value" }
     ]
     });
 } );
