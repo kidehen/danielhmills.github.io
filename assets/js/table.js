5a596c30853dd7ec4f7fd6e56271bf4d6c666e6e
Daniel Heward-mills <danielheward-mills@dyn240.usnet.private>
2019-05-10 11:27:30 -0400

    Avengers: Endgame Article Started

--- assets/js/table.js
+++ assets/js/table.js
@@ -43,7 +43,7 @@ $(document).ready( function () {
          "data": "avenger.value",
          "render": function(data, type, row, meta){
             if(type === 'display'){
-                data = '<a href="' + data + '">' + data + '</a>';
+                data = '<a href="http://example.com/' + data + '">' + data + '</a>';
             }
 
             return data;
