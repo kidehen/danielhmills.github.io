62066cd43c71f873524191b0e47a3dab56333478
Daniel Heward-mills <danielheward-mills@dyn240.usnet.private>
2019-05-09 20:00:57 -0400

    Avengers: Endgame Article Started

--- assets/data/insert.sql
+++ assets/data/insert.sql
@@ -129,4 +129,123 @@ endgame:heroes <#captainmarvel>,
 endgame:villains
                 <#thanos>.
 
+
+#Infinity Stones
+
+<#powerstone>
+a endgame:InfinityStone;
+schema:name "Power Stone";
+endgame:casedIn <#orb>;
+foaf:depiction <https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/d/d7/Power_Stone_VFX.png/revision/latest?cb=20190427012543>.
+
+<#powerstoneheist>
+a endgame:Event;
+schema:name "Power Stone Heist";
+endgame:location <#morag>;
+endgame:year "2012";
+endgame:pastFilm <#guardiansofthegalaxy>;
+endgame:target <#powerstone>;
+endgame:heistMembers <#nebula>, <#warmachine>;
+endgame:heistSuccess "true"^^xsd:boolean.
+
+
+<#spacestone>
+a endgame:InfinityStone;
+schema:name "Space Stone";
+endgame:casedIn <#tesseract>;
+foaf:depiction <https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/0/0a/Space_Stone_VFX.png/revision/latest?cb=20190427012702>.
+
+
+<#spacestoneheist-1>
+a endgame:Event;
+schema:name "Space Stone Heist #1";
+endgame:location <#nyc>;
+endgame:year "2012";
+endgame:pastFilm <#theavengers-film>;
+endgame:target <#spacestone>;
+endgame:heistMembers <#ironman>, <#antman>;
+endgame:heistSuccess "false"^^xsd:boolean.
+
+<#spacestoneheist-2>
+a endgame:Event;
+schema:name "Space Stone Heist #2";
+endgame:location <#camplehigh>;
+endgame:year "1970";
+endgame:target <#spacestone>;
+endgame:pastFilm <#theavengers-film>;
+endgame:heistMembers <#ironman>, <#captainamerica>;
+endgame:heistSuccess "true"^^xsd:boolean.
+
+
+<#mindstone>
+a endgame:InfinityStone;
+schema:name "Mind Stone";
+endgame:casedIn <#scepter>;
+foaf:depiction <https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/e/e4/Mind_Stone_VFX.png/revision/latest?cb=20190427012504>.
+
+
+<#mindstoneheist>
+a endgame:Event;
+schema:name "Mind Stone Heist";
+endgame:location <#nyc>;
+endgame:year "2012";
+endgame:pastFilm <#theavengers-film>;
+endgame:target <#mindstone>;
+endgame:heistMembers <#captainamerica>;
+endgame:heistSuccess "true"^^xsd:boolean.
+
+
+<#soulstone>
+a endgame:InfinityStone;
+schema:name "Soul Stone";
+foaf:depiction <https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/1/17/Soul_Stone_VFX.png/revision/latest?cb=20190427012633>.
+
+
+<#soulstoneheist>
+a endgame:Event;
+schema:name "Soul Stone Heist";
+endgame:location <#vormir>;
+endgame:year "2014";
+endgame:pastFilm <#infinitywar>;
+endgame:target <#soulstone>;
+endgame:heistMembers <#hawkeye>,<#blackwidow>;
+endgame:heistSuccess "true"^^xsd:boolean.
+
+
+<#timestone>
+a endgame:InfinityStone;
+schema:name "Time Stone";
+endgame:casedIn <#eyeofagomotto>;
+foaf:depiction <https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/f/f0/Time_Stone_VFX.png/revision/latest?cb=20190427012724>.
+
+
+<#timestoneheist>
+a endgame:Event;
+schema:name "Time Stone Heist";
+endgame:location <#nyc>;
+endgame:year "2012";
+endgame:pastFilm <#theavengers-film>;
+endgame:target <#timestone>;
+endgame:heistMembers <#hulk>;
+endgame:heistSuccess "true"^^xsd:boolean.
+
+
+<#realitystone>
+a endgame:InfinityStone;
+schema:name "Reality Stone";
+endgame:casedIn <#ather>;
+foaf:depiction <https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/9b/Reality_Stone_VFX.png/revision/latest?cb=20190427012609>.
+
+
+<#realitystoneheist>
+a endgame:Event;
+schema:name "Reality Stone Heist";
+endgame:location <#asgard>;
+endgame:year "2013";
+endgame:pastFilm <#thorthedarkworld>;
+endgame:target <#realitystone>;
+endgame:heistMembers <#thor>,<#rocket>;
+endgame:heistSuccess "true"^^xsd:boolean.
+
+
 }
